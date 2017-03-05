a918ade4c7386adbf204df96c6167eaec1019c60
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-05 23:58:43 +0200

    2. Bootstrap hybrid app

--- app/todo-app.module.ts
+++ app/todo-app.module.ts
@@ -1,4 +1,7 @@
 import * as angular from 'angular';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {UpgradeModule} from '@angular/upgrade/static';
 import {sharedModule} from './shared/shared.module';
 import {todoItemModule} from './todo-item/todo-item.module';
 import {appComponent} from './todo-app.component';
@@ -10,3 +13,18 @@ export const todoAppModule = angular.
     todoItemModule.name,
   ]).
   component('todoApp', appComponent);
+
+// Main Angular module
+@NgModule({
+  imports: [
+    BrowserModule,
+    UpgradeModule,
+  ],
+})
+export class TodoAppModule {
+  constructor(protected upgrade: UpgradeModule) {}
+
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, [todoAppModule.name]);
+  }
+}
