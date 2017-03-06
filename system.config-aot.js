d1d599b5269e45611c8d9cc982b5d7902f547243
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-06 18:09:56 +0200

    10. Migrate `Item` component

--- system.config-aot.js
+++ system.config-aot.js
@@ -9,6 +9,7 @@
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
