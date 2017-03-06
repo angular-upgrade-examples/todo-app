1bd31918d2528711665463d7d91c8e9066d074ab
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-06 18:12:44 +0200

    11. Final Angular app

--- app/todo-app.module.ts
+++ app/todo-app.module.ts
@@ -1,16 +1,9 @@
-import * as angular from 'angular';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {downgradeComponent, UpgradeModule} from '@angular/upgrade/static';
 import {SharedModule} from './shared/shared.module';
 import {TodoItemModule} from './todo-item/todo-item.module';
 import {AppComponent} from './todo-app.component';
 import {FooterComponent} from './todo-app-footer.component';
-
-// Main AngularJS module
-export const todoAppModule = angular.
-  module('todoApp', []).
-  directive('todoApp', downgradeComponent({component: AppComponent}));
 
 // Main Angular module
 @NgModule({
@@ -18,15 +11,8 @@ export const todoAppModule = angular.
     BrowserModule,
     SharedModule,
     TodoItemModule,
-    UpgradeModule,
   ],
   declarations: [AppComponent, FooterComponent],
-  entryComponents: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class TodoAppModule {
-  constructor(protected upgrade: UpgradeModule) {}
-
-  ngDoBootstrap() {
-    this.upgrade.bootstrap(document.body, [todoAppModule.name]);
-  }
-}
+export class TodoAppModule {}
