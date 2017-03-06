f8f1bd05e5935a941caa9696b40bf85a2901b816
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-06 17:36:14 +0200

    6. Upgrade `Item` component (upgrade component)

--- app/todo-item/todo-item.module.ts
+++ app/todo-item/todo-item.module.ts
@@ -1,7 +1,15 @@
 import * as angular from 'angular';
-import {itemComponent} from './todo-item.component';
+import {NgModule} from '@angular/core';
+import {itemComponent, ItemComponentFacade} from './todo-item.component';
 
 // AngularJS todo-item module
 export const todoItemModule = angular.
   module('todoItem', []).
   component('todoItem', itemComponent);
+
+// Angular todo-item module
+@NgModule({
+  declarations: [ItemComponentFacade],
+  exports: [ItemComponentFacade],
+})
+export class TodoItemModule {}
